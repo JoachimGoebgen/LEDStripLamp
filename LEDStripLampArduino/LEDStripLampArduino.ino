6d32b19f91a701f9a67327dd9a2917bcfe830b29
JoachimGoebgen <36620066+JoachimGoebgen@users.noreply.github.com>
2018-06-17 23:27:20 +0200

    add party-mode

--- LEDStripLampArduino/LEDStripLampArduino.ino
+++ LEDStripLampArduino/LEDStripLampArduino.ino
@@ -53,6 +53,23 @@ void loop()
   FastLED.show();
 }
 
+void modeParty() 
+{
+  uint8_t blurAmount = dim8_raw( beatsin8(3, 64, 192) );       // A sinewave at 3 Hz with values ranging from 64 to 192.
+  blur1d(leds, TOTAL_LEDS, blurAmount);                        // Apply some blurring to whatever's already on the strip, which will eventually go black.
+  
+  uint8_t  i = beatsin8(  9, 0, TOTAL_LEDS);
+  uint8_t  j = beatsin8( 7, 0, TOTAL_LEDS);
+  uint8_t  k = beatsin8(  5, 0, TOTAL_LEDS);
+  
+  // The color of each point shifts over time, each at a different speed.
+  uint16_t ms = millis();  
+  leds[(i+j)/2] = CHSV( ms / 29, 200, 255);
+  leds[(j+k)/2] = CHSV( ms / 41, 200, 255);
+  leds[(k+i)/2] = CHSV( ms / 73, 200, 255);
+  leds[(k+i+j)/3] = CHSV( ms / 53, 200, 255);
+  delay(10);
+}
 
 void modeSolid(uint8_t R[4], uint8_t G[4], uint8_t B[4]) 
 {
