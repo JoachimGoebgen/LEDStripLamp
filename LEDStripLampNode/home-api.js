29f2f3c18db2098e105334e936b6d61f74df5710
JoachimGoebgen <36620066+JoachimGoebgen@users.noreply.github.com>
2018-06-28 00:42:44 +0200

    [Node] saving color-presets to file

--- LEDStripLampNode/home-api.js
+++ LEDStripLampNode/home-api.js
@@ -10,32 +10,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 var settings = new Array(2);
-var led = new Array(12);
-var savedModes = new Array(0);
+var colors = new Array(12);
+var presets = new Array(0);
 
 var CONF_FILE_PATH = '../LEDStripLampArduino/connection_conf.h';
+var PRESET_FILE_PATH = '../saved_modes';
 var MQTT_COLOR_TOPIC;
 var MQTT_SETTINGS_TOPIC;
-var MQTT_SAVE_TOPIC;
 var MQTT_SRV;
 var mqttClient;
 
 initConfig();
 
-Array.prototype.clean = function(deleteValue) {
-  for (var i = 0; i < this.length; i++) {
-    if (this[i] == deleteValue) {
-      this.splice(i, 1);
-      i--;
-    }
-  }
-  return this;
-};
-
 mqttClient.on('connect', () => {
 	mqttClient.subscribe(MQTT_COLOR_TOPIC)
 	mqttClient.subscribe(MQTT_SETTINGS_TOPIC)
-	mqttClient.subscribe(MQTT_SAVE_TOPIC)
 });
 
 mqttClient.on('message', (topic, message) => {
@@ -43,38 +32,42 @@ mqttClient.on('message', (topic, message) => {
 		led = message.toString().split(" ");
 	} else if (topic === MQTT_SETTINGS_TOPIC) {
 		settings = message.toString().split(" ");
-	} else if (topic === MQTT_SAVE_TOPIC) {
-                saveModeStrToFile(message.toString());
-		var newMode = parseModeFromString(message.toString());
-		savedModes.push(newMode);
 	}
 
 	//console.log(message);
 });
 
-app.get('/savedModes', function (req, res) {
-    res.status(200).send(savedModes);
+
+app.get('/colors', function (req, res) {
+        res.send(colors);
 });
 
-app.post('/color', function (req, res) {
-    	led = req.body['colArr[]'];
-	mqttClient.publish(MQTT_COLOR_TOPIC, parseArrayToMsg(led));
-	res.status(200).send(led);
+app.get('/settings', function (req, res) {
+        res.send(settings);
+});
+
+app.get('/presets', function (req, res) {
+    	res.send(presets);
+});
+
+app.post('/colors', function (req, res) {
+        colors = req.body['colorArr[]'];
+        mqttClient.publish(MQTT_COLOR_TOPIC, colors.join(" "));
+        res.status(200).send(colors);
 });
 
 app.post('/settings', function (req, res) {
-    	settings = req.body['settArr[]'];
-        mqttClient.publish(MQTT_SETTINGS_TOPIC, parseArrayToMsg(led));
-    	res.status(200).send(settings);
+        settings = req.body['settingArr[]'];
+        mqttClient.publish(MQTT_SETTINGS_TOPIC, settings.join(" "));
+        res.status(200).send(settings);
 });
 
-function parseArrayToMsg(arr) {
-	
-};
+app.post('/presets', function (req, res) {
+        presets.push(req.body['presetArr[]']);
+        savePresetsToFile();
+        res.status(200).send(presets);
+});
 
-function saveModeStrToFile(modeStr) {
-	fs.appendFileSync(CONF_FILE_PATH, "\n" + modeStr);
-};
 
 function initConfig() {
     	var data = String(fs.readFileSync(CONF_FILE_PATH));
@@ -91,16 +84,30 @@ function initConfig() {
     	//console.log(MQTT_SETTINGS_TOPIC);
 	//console.log(MQTT_SAVE_TOPIC);
 
-	var savedModesStrings = String(fs.readFileSync('../saved_modes')).split(/\n/);
-	for (i = 0; i < savedModesStrings.length; i++) {
-		var newMode = parseModeFromString(savedModesStrings[i]);
-		savedModes.push(newMode);
+	// read presets from file
+	var lines = cleanEmptyEntries(String(fs.readFileSync(PRESET_FILE_PATH)).split(/\n/));
+	for (i = 0; i < lines.length; i++) {
+		var newPreset = parsePresetFromString(lines[i]);
+		presets.push(newPreset);
 	}
 
-	console.log(savedModes);
+	//console.log(presets);
 };
 
-function parseModeFromString(str) {
+function savePresetsToFile() {
+        var lines = new Array(presets.length);
+        for (i = 0; i < presets.length; i++) {
+                var temp = new Array(3);
+                temp[0] = presets[i][0];
+                temp[1] = presets[i][1].join(" ");
+                temp[2] = presets[i][2].join(" ");
+                lines[i] = temp.join(", ");
+        }
+
+        fs.writeFile(PRESET_FILE_PATH, lines.join("\n"));
+};
+
+function parsePresetFromString(str) {
 	var strSplit = str.split(",");
         var newMode = new Array(3);
         newMode[0] = strSplit[0];
